a0295d85fd6dacbce9918eef4efe0348eb4b7546
thetalalahmad <thetalalahmad@gmail.com>
2025-08-07 10:52:24 +0530

    reverse string commit

---
+++ src/ReverseString.java
@@ -0,0 +1,18 @@
+public class ReverseString {
+    public static void main( String[] args) {
+        String str = "aaabbbcccddd";
+        char[] arr = str.toCharArray();
+        int start =0;
+        int end = arr.length -1;
+
+        while(start <end){
+            char temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] =temp;
+            start++;
+            end--;
+        }
+        String reversed = new String(arr);
+        System.out.println(reversed);
+    }
+}
